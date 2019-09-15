a8643aba71bca8f165bf0f02eabeb5eb58cedc29
thiago <tvttavares@gmail.com>
2019-09-15 08:55:34 -0400

    Projeto de exemplo da aula sobre SelectOneMenu do PrimeFaces

---
+++ SelectOneMenu/src/main/java/PerfilUsuarioBean.java
@@ -0,0 +1,42 @@
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
+
+@ManagedBean
+@ViewScoped
+public class PerfilUsuarioBean implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	private String nome;
+	private String profissao;
+
+	public void atualizar() {
+		System.out.println("Profissão: " + this.profissao);
+
+		FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Perfil atualizado!"));
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getProfissao() {
+		return profissao;
+	}
+
+	public void setProfissao(String profissao) {
+		this.profissao = profissao;
+	}
+
+}
