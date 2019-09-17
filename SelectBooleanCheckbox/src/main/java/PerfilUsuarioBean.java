c7ca7b6592a3112e0a9fc834739bbd0ca862b42d
thiago <tvttavares@gmail.com>
2019-09-16 22:20:36 -0400

    Projeto de exemplo da aula sobre o componente SelectBooleanCheckbox do PrimeFaces

---
+++ SelectBooleanCheckbox/src/main/java/PerfilUsuarioBean.java
@@ -0,0 +1,42 @@
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
+
+@ManagedBean
+@ViewScoped
+public class PerfilUsuarioBean implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	private String nome;
+	private Boolean aceito;
+
+	public void atualizar() {
+		System.out.println("Aceito: " + this.aceito);
+
+		FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Perfil atualizado!"));
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Boolean getAceito() {
+		return aceito;
+	}
+
+	public void setAceito(Boolean aceito) {
+		this.aceito = aceito;
+	}
+
+}
