36808de1502399e99da4e9061d8c871934998138
thiago <tvttavares@gmail.com>
2019-10-12 18:47:44 -0400

    Exercicio: Inclusao de Menus e Dialogos de confirmacao para Clientes e Usuarios

---
+++ AjusteMenuEDialogosConfirmacaoUsuarioCliente/src/main/java/com/algaworks/pedidovenda/controller/PesquisaClientesBean.java
@@ -0,0 +1,25 @@
+package com.algaworks.pedidovenda.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+@ManagedBean
+@RequestScoped
+public class PesquisaClientesBean {
+
+	private List<Integer> clientesFiltrados;
+
+	public PesquisaClientesBean() {
+		clientesFiltrados = new ArrayList<>();
+		for (int i = 0; i < 10; i++) {
+			clientesFiltrados.add(i);
+		}
+	}
+
+	public List<Integer> getClientesFiltrados() {
+		return clientesFiltrados;
+	}
+}
