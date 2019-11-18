f7f602137e35745db78301bdea58145b8d89a024
thiago <tvttavares@gmail.com>
2019-11-17 22:44:32 -0400

    Desafio: enviando e-mail

---
+++ EnvioEmailCadastroCliente/src/main/java/Teste2.java
@@ -0,0 +1,32 @@
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+import com.algaworks.pedidovenda.model.Grupo;
+import com.algaworks.pedidovenda.model.Usuario;
+
+public class Teste2 {
+	public static void main(String[] args) {
+		EntityManagerFactory factory = Persistence.createEntityManagerFactory("PedidoPU");
+		EntityManager manager = factory.createEntityManager();
+
+		EntityTransaction trx = manager.getTransaction();
+		trx.begin();
+
+		Usuario usuario = new Usuario();
+		usuario.setNome("Maria");
+		usuario.setEmail("maria@abadia.com");
+		usuario.setSenha("123");
+
+		Grupo grupo = new Grupo();
+		grupo.setNome("Vendedores");
+		grupo.setDescricao("Vendedores da empresa");
+
+		usuario.getGrupos().add(grupo);
+
+		manager.persist(usuario);
+
+		trx.commit();
+	}
+}
---
+++ EnvioEmailCadastroCliente/src/main/java/com/algaworks/pedidovenda/model/TipoPessoa.java
@@ -0,0 +1,17 @@
+package com.algaworks.pedidovenda.model;
+
+public enum TipoPessoa {
+
+	FISICA("Física"), JURIDICA("Jurídica");
+
+	private String descricao;
+
+	TipoPessoa(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+}
