b0a32049e14cd6fa0e54e44cb2560a14d90f3fe2
thiago <tvttavares@gmail.com>
2019-11-21 20:43:11 -0300

    Populando o gráfico com dados dinâmicos

--- PopulandoGraficoDadosDinamicos/src/main/java/com/algaworks/pedidovenda/controller/GraficoPedidosCriadosBean.java
+++ PopulandoGraficoDadosDinamicos/src/main/java/com/algaworks/pedidovenda/controller/GraficoPedidosCriadosBean.java
@@ -23,37 +23,37 @@ import com.algaworks.pedidovenda.security.UsuarioSistema;
 public class GraficoPedidosCriadosBean {
 
 	private static DateFormat DATE_FORMAT = new SimpleDateFormat("dd/MM");
-
+	
 	@Inject
 	private Pedidos pedidos;
-
+	
 	@Inject
 	@UsuarioLogado
 	private UsuarioSistema usuarioLogado;
-
+	
 	private CartesianChartModel model;
 
 	public void preRender() {
 		this.model = new CartesianChartModel();
-
+		
 		adicionarSerie("Todos os pedidos", null);
 		adicionarSerie("Meus pedidos", usuarioLogado.getUsuario());
 	}
-
+	
 	private void adicionarSerie(String rotulo, Usuario criadoPor) {
 		Map<Date, BigDecimal> valoresPorData = this.pedidos.valoresTotaisPorData(15, criadoPor);
-
+		
 		ChartSeries series = new ChartSeries(rotulo);
-
+		
 		for (Date data : valoresPorData.keySet()) {
 			series.set(DATE_FORMAT.format(data), valoresPorData.get(data));
 		}
-
+		
 		this.model.addSeries(series);
 	}
 
 	public CartesianChartModel getModel() {
 		return model;
 	}
-
+	
 }
--- PopulandoGraficoDadosDinamicos/src/main/java/com/algaworks/pedidovenda/security/Seguranca.java
+++ PopulandoGraficoDadosDinamicos/src/main/java/com/algaworks/pedidovenda/security/Seguranca.java
@@ -15,16 +15,16 @@ public class Seguranca {
 
 	@Inject
 	private ExternalContext externalContext;
-
+	
 	public String getNomeUsuario() {
 		String nome = null;
-
+		
 		UsuarioSistema usuarioLogado = getUsuarioLogado();
-
+		
 		if (usuarioLogado != null) {
 			nome = usuarioLogado.getUsuario().getNome();
 		}
-
+		
 		return nome;
 	}
 
@@ -32,23 +32,25 @@ public class Seguranca {
 	@UsuarioLogado
 	public UsuarioSistema getUsuarioLogado() {
 		UsuarioSistema usuario = null;
-
-		UsernamePasswordAuthenticationToken auth = (UsernamePasswordAuthenticationToken) FacesContext
-				.getCurrentInstance().getExternalContext().getUserPrincipal();
-
+		
+		UsernamePasswordAuthenticationToken auth = (UsernamePasswordAuthenticationToken) 
+				FacesContext.getCurrentInstance().getExternalContext().getUserPrincipal();
+		
 		if (auth != null && auth.getPrincipal() != null) {
 			usuario = (UsuarioSistema) auth.getPrincipal();
 		}
-
+		
 		return usuario;
 	}
-
+	
 	public boolean isEmitirPedidoPermitido() {
-		return externalContext.isUserInRole("ADMINISTRADORES") || externalContext.isUserInRole("VENDEDORES");
+		return externalContext.isUserInRole("ADMINISTRADORES") 
+				|| externalContext.isUserInRole("VENDEDORES");
 	}
-
+	
 	public boolean isCancelarPedidoPermitido() {
-		return externalContext.isUserInRole("ADMINISTRADORES") || externalContext.isUserInRole("VENDEDORES");
+		return externalContext.isUserInRole("ADMINISTRADORES") 
+				|| externalContext.isUserInRole("VENDEDORES");
 	}
-
+	
 }
